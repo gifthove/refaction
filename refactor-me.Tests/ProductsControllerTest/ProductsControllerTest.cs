a293aad40be49824b3cf50082dae962d28e90172
Gift Hove <Gift.Hove@GIFTHOVE>
2017-10-23 13:03:37 +0200

    Included all the missing files  and Tests

--- refactor-me.Tests/ProductsControllerTest/ProductsControllerTest.cs
+++ refactor-me.Tests/ProductsControllerTest/ProductsControllerTest.cs
@@ -162,5 +162,74 @@ namespace refactor_me.Tests
             Assert.AreEqual(updatedproduct.Name, "Testing MockTest");
 
         }
+
+        /// <summary>
+        /// Creates the option create a product option via service.
+        /// </summary>
+        [TestMethod]
+        public void CreateOption_Create_a_ProductOption_via_service()
+        {
+            //Arrange 
+            var productOption = new core.Models.ProductOption { ProductId = new Guid("de1287c0-4b15-4a7b-9d8a-dd21b3cafec3"), Name = "Rose Gold", Description = "Gold Apple iPhone 6S" };
+
+            //Act 
+            _Controller.CreateOption(new Guid("de1287c0-4b15-4a7b-9d8a-dd21b3cafec3"),productOption);
+            var count = _mockContext.ProductOptions.Local.Count;
+
+            //Assert
+            Assert.AreEqual(count, 4);
+
+        }
+
+        /// <summary>
+        /// Gets the options gets by product a product option via service.
+        /// </summary>
+        [TestMethod]
+        public void GetOptions_gets_By_Product_a_ProductOption_via_Service()
+        {
+
+            //Act 
+            var productOption = _Controller.GetOption(new Guid("8f2e9176-35ee-4f0a-ae55-83023d2db1a3"),new Guid("0643ccf0-ab00-4862-b3c5-40e2731abcc9"));
+
+            //Assert
+            Assert.IsNotNull(productOption);
+            Assert.AreEqual(productOption.Name, "White");
+
+        }
+
+        /// <summary>
+        /// Gets the options gets all product options via service.
+        /// </summary>
+        [TestMethod]
+        public void GetOptions_gets_all_ProductOptions_via_Service()
+        {
+            //Act 
+            var productOptions = _Controller.GetOptions(new Guid("8f2e9176-35ee-4f0a-ae55-83023d2db1a3")); 
+
+            //Assert
+            Assert.IsNotNull(productOptions.Items);
+            Assert.AreEqual(productOptions.Items.Count, 2);
+
+        }
+
+        /// <summary>
+        /// Updates the option update specified product option via service.
+        /// </summary>
+        [TestMethod]
+        public void UpdateOption_update_specified_ProductOption_via_Service()
+        {
+            //Arrange
+            var productOptionToUpdate = new core.Models.ProductOption { Id = new Guid("5c2996ab-54ad-4999-92d2-89245682d534"), ProductId = new Guid("de1287c0-4b15-4a7b-9d8a-dd21b3cafec3"), Name = "Rose Gold Test Mock", Description = "Gold Apple iPhone 6S" };
+
+            //Act 
+            _Controller.UpdateOption(new Guid("5c2996ab-54ad-4999-92d2-89245682d534"),productOptionToUpdate);
+            var updatedProductOption = _Controller.GetOption(new Guid("de1287c0-4b15-4a7b-9d8a-dd21b3cafec3"),new Guid("5c2996ab-54ad-4999-92d2-89245682d534"));
+
+            //Assert
+            Assert.IsNotNull(updatedProductOption);
+            Assert.AreEqual(updatedProductOption.Name, "Rose Gold Test Mock");
+            Assert.AreEqual(updatedProductOption.Name, productOptionToUpdate.Name);
+        }
+      
     }
 }
